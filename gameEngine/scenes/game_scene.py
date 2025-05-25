becc38d93c83cfa3fb6d3b2b694785f53e64b624
LlechiKaito <124239629+LlechiKaito@users.noreply.github.com>
2025-05-25 22:41:38 +0900

    feat: ゲームシーンから編集画面への遷移の追加

--- gameEngine/scenes/game_scene.py
+++ gameEngine/scenes/game_scene.py
@@ -71,8 +71,9 @@ class GameScene(BaseScene):
 
         if pyxel.btnp(pyxel.KEY_T): # 例: Tキーでタイトルシーンへ遷移
             self.scene_manager.change_scene('title')
-        if pyxel.btnp(pyxel.KEY_M):
-            self.scene_manager.change_scene('menu')
+        
+        if pyxel.btnp(pyxel.KEY_E): # 例: Eキーでタイトルシーンへ遷移
+            self.scene_manager.change_scene('edit')
 
 
     def draw_checkerboard(self, x_offset, y_offset, width, height, color1, color2):
